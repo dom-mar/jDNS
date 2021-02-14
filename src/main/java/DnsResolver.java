df0c5b7e5da7a56ff8148c710613b282fe0497d8
Dominik Marjanović <dominik.marjanovic@gmail.com>
2021-02-14 22:13:32 +0100

    Clean up DNSResolver and add comments

--- src/main/java/DnsResolver.java
+++ src/main/java/DnsResolver.java
@@ -1,29 +1,13 @@
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.Inet4Address;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Arrays;
 
 public class DnsResolver {
 
     public static void main(String[] args) throws Exception {
-//        Path path = Paths.get("C:\\Users\\Dominik\\Documents\\google_dns_response.txt");
-//        BytePacketBuffer buffer = new BytePacketBuffer();
-//        buffer.setBuffer(Files.readAllBytes(path));
-//
-//
-//        DnsPacket packet = new DnsPacket();
-//        packet.fromBuffer(buffer);
-//
-//
-//        System.out.println(packet.getHeader());
-//        packet.getQuestions().forEach(System.out::println);
-//        packet.getAnswers().forEach(System.out::println);
-//        packet.getAuthorityRecords().forEach(System.out::println);
-//        packet.getResourceRecords().forEach(System.out::println);
 
+        // Domain that is being resolved
         String qname = "www.yahoo.com";
         QueryType queryType = QueryType.A;
 
@@ -31,36 +15,56 @@ public class DnsResolver {
         String server = "8.8.8.8";
         int port = 53;
 
+        // Create new UDP socket
         DatagramSocket socket = new DatagramSocket();
 
+
         DnsPacket packet = new DnsPacket();
+
+        // Set random ID - it doesn't matter
         packet.getHeader().setId((short) 7654);
         packet.getHeader().setQuestionsCnt((short) 1);
+        // Set to true so Google's DNS can resolve it
         packet.getHeader().setRecursionDesired(true);
 
-        packet.getQuestions().add(new DnsQuestion(qname,queryType));
+        // Create new question for A record of 'www.yahoo.com'
+        packet.getQuestions().add(new DnsQuestion(qname, queryType));
         BytePacketBuffer requestBuffer = new BytePacketBuffer();
 
+        // Write newly created DNS packet to buffer
         packet.write(requestBuffer);
 
-        DatagramPacket udpPacket = new DatagramPacket(Arrays.copyOfRange(requestBuffer.getBuffer(), 0, requestBuffer.getPosition()), requestBuffer.getPosition(), Inet4Address.getByName(server),port);
+        // Create UDP packet by copying part of the request buffer
+        // up till the current position inside that buffer.
+        // Size of copied buffer is equal to current position
+        // Use 8.8.8.8 as address and 53 as port for UDP packet
+        DatagramPacket udpPacket = new DatagramPacket(Arrays.copyOfRange(requestBuffer.getBuffer(),
+                0, requestBuffer.getPosition()), requestBuffer
+                .getPosition(), Inet4Address.getByName(server), port);
+
+        // Send UDP packet
         socket.send(udpPacket);
 
+        // Create new buffer for response
         BytePacketBuffer responseBuffer = new BytePacketBuffer();
-        udpPacket = new DatagramPacket(responseBuffer.getBuffer(),responseBuffer.getBuffer().length);
 
+        // Set UDP packet's buffer to newly created one
+        udpPacket = new DatagramPacket(responseBuffer.getBuffer(), responseBuffer.getBuffer().length);
+
+        // Receive data from Google's DNS resolver
         socket.receive(udpPacket);
 
+        // Create new DNS packet and fill it with data from
+        // response buffer
         DnsPacket responsePacket = new DnsPacket();
         responsePacket.fromBuffer(responseBuffer);
 
+        // Print retrieved information
         System.out.println(packet.getHeader());
         responsePacket.getQuestions().forEach(System.out::println);
         responsePacket.getAnswers().forEach(System.out::println);
         responsePacket.getAuthorityRecords().forEach(System.out::println);
         responsePacket.getResourceRecords().forEach(System.out::println);
-
-
 
 
     }
